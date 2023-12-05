2b9153999f3440ed805cc33bfdb6fc725836ceb0
Tait Schaffer <tait.schaffer@gmail.com>
2023-12-05 05:07:43 -0800

    Update template
    The changes between parts one and two demand more than just a
    single-line code change, so add it in as a parameter now.

--- dayNN.cpp
+++ dayNN.cpp
@@ -7,7 +7,6 @@
 #include <iostream>
 #include <map>
 #include <numeric>
-#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -30,6 +29,74 @@ typename std::enable_if_t<std::is_integral<TT>::value> dump(const std::vector<TT
 	for (size_t aa=0; aa<input.size(); aa++) {
 		std::cout << aa << ": " << input[aa] << std::endl;
 	}
+}
+
+template <typename TT>
+typename std::enable_if_t<! std::is_integral<TT>::value, std::string>
+join(const std::vector<TT>& input, const std::string& delim=" ") {
+	std::string result;
+	bool first = true;
+	for (const auto& aa : input) {
+		if (first) {
+			first = false;
+		}
+		else {
+			result += delim;
+		}
+		result += aa.str();
+	}
+	return result;
+}
+template <typename TT>
+typename std::enable_if_t<std::is_integral<TT>::value, std::string>
+join(const std::vector<TT>& input, const std::string& delim=" ") {
+	std::string result;
+	bool first = true;
+	for (const auto& aa : input) {
+		if (first) {
+			first = false;
+		}
+		else {
+			result += delim;
+		}
+		result += std::to_string(aa);
+	}
+	return result;
+}
+template <>
+std::string join(const std::vector<std::string>& input, const std::string& delim) {
+	std::string result;
+	bool first = true;
+	for (const auto& aa : input) {
+		if (first) {
+			first = false;
+		}
+		else {
+			result += delim;
+		}
+		result += aa;
+	}
+	return result;
+}
+
+std::vector<std::string> split(const std::string& input, const std::string& delim=" ") {
+	std::vector<std::string> results;
+	size_t pos = 0;
+	size_t ppos = pos;
+	while ((pos = input.find(delim, ppos)) != input.npos) {
+		results.emplace_back(input.substr(ppos, pos-ppos));
+		ppos = pos + delim.size();
+	}
+	if (ppos < input.size()) {
+		results.emplace_back(input.substr(ppos));
+	}
+	return results;
+}
+
+std::string str_tolower(std::string input) {
+	std::transform(input.begin(), input.end(), input.begin(),
+		[](unsigned char cc) { return std::tolower(cc); });
+	return input;
 }
 
 std::vector<std::string> get_input() {
@@ -57,13 +124,50 @@ std::vector<std::string> get_input(const std::string& file) {
 	return input;
 }
 
+int part1_answer() {
+	return 0;
+}
+
+int part2_answer() {
+	return 0;
+}
+
 int main(int argc, char* argv[]) {
 	std::vector<std::string> input;
-	if (argc > 1) {
+	int part = 2;
+	if (argc > 2) {
+		try {
+			part = std::stoi(argv[2]);
+		}
+		catch (...) {
+			std::cerr << "Error: invalid part " << argv[2] << "; must be 1 or 2" << std::endl;
+			return 1;
+		}
 		input = get_input(argv[1]);
 	}
-	else {
+	else if (argc > 1) {
+		try {
+			part = std::stoi(argv[1]);
+		}
+		catch (...) {
+			std::cerr << "Error: invalid part " << argv[1] << "; must be 1 or 2" << std::endl;
+			return 1;
+		}
 		input = get_input();
+	}
+	else {
+		std::cerr << "Usage: " << argv[0] << " [input-file] <1|2>" << std::endl;
+		return 1;
+	}
+	if (! ((part == 1) || (part == 2))) {
+		std::cerr << "Error: invalid part " << part << "; must be 1 or 2" << std::endl;
+		return 1;
+	}
+	if (part == 2) {
+		std::cout << part2_answer() << std::endl;
+	}
+	else {
+		std::cout << part1_answer() << std::endl;
 	}
 	std::cout << "done" << std::endl;
 	return 0;
